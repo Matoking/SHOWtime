3b9c57cc1448efd725b1706e864213e1351d85e5
Janne Pulkkinen <jannepulk@gmail.com>
2014-12-29 13:19:09 +0200

    Fix config.py so that SHOWtime can be run out-of-the-box

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ import os
 
 # Add any tabs you want to be visible here
 tabs = [ # Track a running Bitcoin node
-         Bitcoind({"host": "http://127.0.0.1:8332",
-                   "username": "bitcoinrpc",
-                   # Read the password from a file
-                   "password": open("bitcoinrpc_password.txt", "r").read().replace("\n", "") }),
+         #Bitcoind({"host": "http://127.0.0.1:8332",
+         #          "username": "bitcoinrpc",
+         #          # Read the password from a file
+         #          "password": "passwordhere" }),
         
          # A Bitcoin price ticker
          BitcoinPrice(),
