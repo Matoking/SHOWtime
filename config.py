882e3088dda841ff582fa1e6d281eb3d058fe8ba
Janne Pulkkinen <jannepulk@gmail.com>
2015-02-02 22:19:11 +0200

    Revert config.py to a state where it runs out-of-the-box

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ import os
 
 # Add any tabs you want to be visible here
 tabs = [ # Track a running Bitcoin node
-         Bitcoind({"host": "http://127.0.0.1:8332",
-                   "username": "bitcoinrpc",
-                   # Read the password from a file
-                   "password": open("bitcoinrpc_password.txt", "r").read().replace("\n", "") }),
+         #Bitcoind({"host": "http://127.0.0.1:8332",
+         #          "username": "bitcoinrpc",
+         #          # Read the password from a file
+         #          "password": open("bitcoinrpc_password.txt", "r").read().replace("\n", "") }),
         
          # A Bitcoin price ticker
          BitcoinPrice(),
